5d07b64740b6542c8fad6c8046980dbe5e215abe
vernonthedev <techjaja2@gmail.com>
2025-11-17 18:20:41 +0300

    chore: setup the main grpc transport protocol

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { AUTH } from '@app/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        protoPath: join(__dirname, '../auth.proto'),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        package: AUTH,
+      },
+    },
+  );
+  await app.listen();
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
